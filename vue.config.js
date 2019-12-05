22b67980a32db623e2d1e24d298139305626ad82
R-Von <778615648@qq.com>
2019-12-05 10:34:04 +0800

    feat: add some config

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  configureWebpack:{
+    module:{
+      rules: [
+        {
+          test: /\.less$/,
+          use: [
+            {
+              loader: 'less-loader',
+              options: {
+                modifyVars: {
+                  // 直接覆盖变量
+                  red: '#03a9f4',
+                  blue: '#3eaf7c',
+                  orange: '#f08d49',
+                  'text-color': '#111',
+                  'border-color': '#eee'
+                  // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
